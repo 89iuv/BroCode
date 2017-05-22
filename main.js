5ee99681a217a4969b03f39c7ace91c8478bb5c7
Valentin Uveges <valentin.uveges@gmail.com>
2017-05-22 22:16:28 +0200

    update port

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.static('public'));
 
 
-var server = app.listen(80, function () {
+var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
 
